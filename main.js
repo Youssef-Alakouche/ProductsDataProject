bd038a16c824c7f453fcaf5c311021931ea4f7ca
Youssef Alakouche <youssef.alakouche@gmail.com>
2023-08-28 01:37:11 +0100

    change

--- main.js
+++ main.js
@@ -8,28 +8,28 @@ const { getExcelFileFromH2HubWatches } = require("./h2hubwatches");
     await getExcelFileFromCityChain();
     console.log("City Chain: finish");
   } catch {
-    console.log("You Have a problem on city chain");
+    console.log("city chain");
   }
 
   console.log("\n66666666666666666666666666666666666666666\n");
 
-  // try {
-  console.log("Thongsia : Start");
-  await getExcelFileFromThongsia();
-  console.log("Thongsia : finish");
-  // } catch {
-  //   console.log("You have a problem on thongsia");
-  // }
+  try {
+    console.log("Thongsia : Start");
+    await getExcelFileFromThongsia();
+    console.log("Thongsia : finish");
+  } catch {
+    console.log("thongsia");
+  }
 
   console.log("\n66666666666666666666666666666666666666666\n");
 
-  // try {
-  console.log("H2 Hub Watches : Start");
-  await getExcelFileFromH2HubWatches();
-  console.log("H2 Hub Watches : finish");
-  // } catch {
-  //   console.log("You have a problem on H2Hubwatches");
-  // }
+  try {
+    console.log("H2 Hub Watches : Start");
+    await getExcelFileFromH2HubWatches();
+    console.log("H2 Hub Watches : finish");
+  } catch {
+    console.log("H2Hubwatches");
+  }
 
   return true;
 })();
